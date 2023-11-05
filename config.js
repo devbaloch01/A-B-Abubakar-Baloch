5b557c85789904200189e1af62fe35a50ab6aee5
devbaloch01 <145318883+devbaloch01@users.noreply.github.com>
2023-11-05 10:19:27 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,50 @@
+const fs = require('fs-extra')
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER.split(",")
+global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.port= process.env.PORT || 5000
+global.email = 'sam@secktor.live'
+global.github = 'https://github.com/SamPandey001/Secktor-Md'
+global.location = 'Sultanpur IN'
+global.gurl = 'https://instagram.com/' // add your username
+global.sudo = process.env.SUDO || '923461253726'
+global.devs = '923173547413';
+global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
+module.exports = {
+  botname: process.env.BOT_NAME || 'A B Abubakar Baloch',
+  ownername:process.env.OWNER_NAME || 'Devbaloch',
+  sessionName: process.env.SESSION_ID || 'PUT-HERE',
+  author: process.env.PACK_INFO.split(";")[0] || 'author', 
+  auto_read_status : process.env.AUTO_READ_STATUS || 'false',
+  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
+  autoreaction: process.env.AUTO_REACTION || 'off',
+  antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'true',
+  antifake : process.env.FAKE_COUNTRY_CODE || '',
+  readmessage: process.env.READ_MESSAGE || false,
+  HANDLERS: process.env.PREFIX || ['.'],
+  warncount : process.env.WARN_COUNT || 3,
+  disablepm: process.env.DISABLE_PM || "flase",
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+  antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
+  antilinkaction: process.env.ANTILINK_ACTION || 'remove',
+  BRANCH: 'main',
+  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  LANG: process.env.THEME|| 'SECKTOR',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
